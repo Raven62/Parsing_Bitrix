d4701b029724bd64b439352a3f5625097d8541c8
Никита <Panc-Pou@yandex.ru>
2021-07-07 23:03:58 +1000

    Комент к коду

--- parsing.php
+++ parsing.php
@@ -1,41 +1,42 @@
 
 результат работы...<br>
 <?
- $handle = fopen($_SERVER['DOCUMENT_ROOT']."/local/test.csv", "r");
- if ($handle) {
-     $counter = 0;
-     $keys = array();
-     $data = array();
-     while (($buffer = fgets($handle)) !== false) {
-         $counter++; 
+ $handle = fopen($_SERVER['DOCUMENT_ROOT']."/local/test.csv", "r"); // Подключение в переменную файла csv
+ if ($handle) { // Если ли в файле значения
+     $counter = 0; // счетчик
+     $keys = array(); //массив 
+
+     $data = array(); //массив 
+     while (($buffer = fgets($handle)) !== false) { // пока прочитанная строка есть выполняем цикл
+         $counter++; // шаг +1;
          $buffer = str_replace(array("\r\n", "\r", "\n"), '', $buffer); 
-         $str =explode(";", $buffer);
+         $str =explode(";", $buffer); // делим строку разделитель ; 
          if ($counter==1){
              $keys = $str;
          }
          else{
              $el = array();
-             foreach ($str as $key=>$item){
-                $el[$keys[$key]] = $item;
+             foreach ($str as $key=>$item){ // ищем из массива 
+                $el[$keys[$key]] = $item; // то чно не понимаю как это происходить. переменной el присваивем значение item с номером
              }
-         $data[] = $el;
+         $data[] = $el; // записываем элементы в массив дата
      }
  }
-    if (!feof($handle)) {
+    if (!feof($handle)) { //если не достигнут конец файла выводим ошибку
         echo "Error: unexpected fgets() fail\n";
     }
-    fclose($handle);
+    fclose($handle); // Закрывает  файл
     
      
-    CModule::IncludeModule("iblock");
+    CModule::IncludeModule("iblock"); // подключение информационных блоков
     
     foreach ($data as $key=>$el){
-        $bs = new CIBlockElement;
-        $PROP = array();
+        $bs = new CIBlockElement; // создание экземпляра класса CIBlockElement
+        $PROP = array(); // создание массива свойств
         $PROP["prop1"] = $el["prop1"]; 
         $PROP["prop2"] = $el["prop2"];
 
-        $arFields = Array(
+        $arFields = Array( // массив с заполниными данными для инфо блока
         "IBLOCK_ID" => 7,
         "NAME" => $el["name"],
         "XML_ID" => $el["id"],
@@ -43,15 +44,15 @@
         "DETAIL_TEXT" => $el["detail_text"],
         "PROPERTY_VALUES"=> $PROP,
         );
-        $res = CIBlockElement::GetByID($el["id"]);
+        
 
-        if($PRODUCT_ID = $bs->Add($arFields))
+        if($PRODUCT_ID = $bs->Add($arFields)) // добовление файла в инфоблок
         {
-            echo 'New ID: '.$PRODUCT_ID.'<br>';
+            echo 'New ID: '.$PRODUCT_ID.'<br>'; //вывод id
         }
         else
         {
-            echo '.Error: '.$bs->LAST_ERROR.'<br>';
+            echo '.Error: '.$bs->LAST_ERROR.'<br>'; // вывод ошибки
         }
     }
     }
