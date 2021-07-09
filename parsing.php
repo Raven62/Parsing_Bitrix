4b2b1dfa2a39fc4071a1ffdbf8d9e9b3b515c0fe
Никита <Panc-Pou@yandex.ru>
2021-07-09 12:30:23 +1000

    Новый парсер

--- parsing.php
+++ parsing.php
@@ -1,42 +1,69 @@
 
 результат работы...<br>
-<?
- $handle = fopen($_SERVER['DOCUMENT_ROOT']."/local/test.csv", "r"); // Подключение в переменную файла csv
- if ($handle) { // Если ли в файле значения
-     $counter = 0; // счетчик
-     $keys = array(); //массив 
+<?php
 
-     $data = array(); //массив 
-     while (($buffer = fgets($handle)) !== false) { // пока прочитанная строка есть выполняем цикл
-         $counter++; // шаг +1;
+ $handle = fopen($_SERVER['DOCUMENT_ROOT']."/local/test.csv", "r");
+ if ($handle) {
+     $counter = 0;
+     $keys = array();
+     $data = array();
+     while (($buffer = fgets($handle)) !== false) {
+         $counter++; 
          $buffer = str_replace(array("\r\n", "\r", "\n"), '', $buffer); 
-         $str =explode(";", $buffer); // делим строку разделитель ; 
+         $str =explode(";", $buffer);
          if ($counter==1){
              $keys = $str;
          }
          else{
              $el = array();
-             foreach ($str as $key=>$item){ // ищем из массива 
-                $el[$keys[$key]] = $item; // то чно не понимаю как это происходить. переменной el присваивем значение item с номером
+             foreach ($str as $key=>$item){
+                $el[$keys[$key]] = $item;
              }
-         $data[] = $el; // записываем элементы в массив дата
+         $data[] = $el;
      }
  }
-    if (!feof($handle)) { //если не достигнут конец файла выводим ошибку
+    if (!feof($handle)) {
         echo "Error: unexpected fgets() fail\n";
     }
-    fclose($handle); // Закрывает  файл
+    fclose($handle);
     
      
-    CModule::IncludeModule("iblock"); // подключение информационных блоков
+    CModule::IncludeModule("iblock");
     
-    foreach ($data as $key=>$el){
-        $bs = new CIBlockElement; // создание экземпляра класса CIBlockElement
-        $PROP = array(); // создание массива свойств
+    foreach ($data as $key=>$el)
+    {
+        
+        $y = false;
+       
+
+        $bs = new CIBlockElement;
+        $arFilter = array(
+            "IBLOCK_ID" => 7,
+        );
+
+        $res = CIBlockElement::GetList(array(), $arFilter, false, false, array('ID','NAME','XML_ID'));
+
+        while ($element = $res->GetNext()) {
+            
+            
+            if($el['name'] == $element['NAME']) 
+            {
+            
+               // echo 'Элемент CSV - '.$el['name'].' '.'Элемент блока - '.$element['NAME'].'<br>';
+             $y = true;
+              break;
+
+            }
+            
+            
+        }
+
+
+        $PROP = array();
         $PROP["prop1"] = $el["prop1"]; 
         $PROP["prop2"] = $el["prop2"];
 
-        $arFields = Array( // массив с заполниными данными для инфо блока
+        $arFields = Array(
         "IBLOCK_ID" => 7,
         "NAME" => $el["name"],
         "XML_ID" => $el["id"],
@@ -44,17 +71,27 @@
         "DETAIL_TEXT" => $el["detail_text"],
         "PROPERTY_VALUES"=> $PROP,
         );
+
+        if ($y == false)
+        {
+           if($PRODUCT_ID = $bs->Add($arFields))
+           {
+           echo $key.'.New ID: '.$PRODUCT_ID.'(XML_ID = cmt_'.$el['id'].')<br>';
+           }
+           else
+           {
+            echo $key.'.Error: '.$bs->LAST_ERROR.'<br>';
+           }
+
+        }
+         
+
+      
+        
+        
         
 
-        if($PRODUCT_ID = $bs->Add($arFields)) // добовление файла в инфоблок
-        {
-            echo 'New ID: '.$PRODUCT_ID.'<br>'; //вывод id
-        }
-        else
-        {
-            echo '.Error: '.$bs->LAST_ERROR.'<br>'; // вывод ошибки
-        }
+     
     }
-    }
- 
-?>
+}
+
